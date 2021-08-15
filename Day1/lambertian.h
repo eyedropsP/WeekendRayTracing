c60f2aa0781867bb65f8b20b5ad4cd6451775f77
eyedrops <eyedropsp@gmail.com>
2021-08-15 15:31:22 +0900

    ぼやけた反射の追加

--- Day1/lambertian.h
+++ Day1/lambertian.h
@@ -9,7 +9,7 @@ class lambertian : public material {
 public:
     explicit lambertian(const color &a) : albedo(a) {}
 
-    virtual bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const {
+    bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override {
         vec3 scatter_direction = rec.normal + random_unit_vector();
         scattered = ray(rec.p, scatter_direction);
         attenuation = albedo;
--- Day1/metal.h
+++ Day1/metal.h
@@ -7,17 +7,18 @@
 
 class metal : public material {
 public:
-    metal(const color &a) : albedo(a) {}
+    explicit metal(const color &a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
-    virtual bool scatter(const ray &r_in, const hit_record &record, color &attenuation, ray &scattered) const {
+    bool scatter(const ray &r_in, const hit_record &record, color &attenuation, ray &scattered) const override {
         vec3 reflected = reflect(unit_vector(r_in.direction()), record.normal);
-        scattered = ray(record.p, reflected);
+        scattered = ray(record.p, reflected + fuzz * random_in_unit_sphere());
         attenuation = albedo;
         return (dot(scattered.direction(), record.normal) > 0);
     }
 
 public:
     color albedo;
+    double fuzz;
 };
 
 #endif //METAL_H
